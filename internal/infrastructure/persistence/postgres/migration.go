1aea1e14f13549271b1006a1163325ea23967c29
Junaidmdv <juni313786@gmail.com>
2026-04-09 20:43:39 +0530

    add database migration

--- internal/infrastructure/persistence/postgres/migration.go
+++ internal/infrastructure/persistence/postgres/migration.go
@@ -1,2 +1,11 @@
-package postgres 
+package postgres
 
+import "github.com/junaidmdv/goalcirlcle/user_service/internal/domain/entity"
+
+func (db *postgressDB) Migration() error {
+	err := db.DB.AutoMigrate(&entity.User{}, &entity.TempUser{})
+	if err != nil {
+		return err
+	}
+	return nil
+}
