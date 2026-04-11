4f9114a475dcb3211f1946dd5be3ab715db0cafe
Junaidmdv <juni313786@gmail.com>
2026-04-11 21:23:30 +0530

    added error interface custom implementation

---
+++ internal/domain/errors.go
@@ -0,0 +1,42 @@
+package domain
+
+import "fmt"
+
+type ErrorType int
+
+const (
+	ErrTypeUnknown ErrorType = iota
+	ErroTypeNotFound
+	ErrorTypeInternal
+	ErrorTypeConflict
+)
+
+type DomainError struct {
+	Type    ErrorType
+	Message string
+	Err     error
+}
+
+func (e *DomainError) Error() string {
+	if e.Err != nil {
+		return fmt.Sprintf("%s:%v", e.Message, e.Err)
+	}
+	return e.Message
+}
+
+func NewNotFoundError(msg string) *DomainError {
+	return &DomainError{Type: ErroTypeNotFound, Message: msg}
+}
+
+func NewInternalError(msg string, err error) *DomainError {
+	return &DomainError{Type: ErrorTypeConflict, Message: msg, Err: err}
+}
+
+func NewConflictError(msg string) *DomainError {
+	return &DomainError{
+		Type:    ErrorTypeConflict,
+		Message: msg,
+	}
+}
+
+
